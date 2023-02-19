4c0b021ab8904124bbe38a734a8e15dbef6dc1ae
Tom Rossner <tomrossner2@gmail.com>
2023-02-19 13:27:37 +0200

    Add getUserInfo function

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const AuthRouter = express.Router();
-const {getUsers, register, login} = require("../controllers/auth.controllers");
+const {getUsers, register, login, getUserInfo} = require("../controllers/auth.controllers");
+const AUTH_MW = require("../middlewares/auth.middleware");
 
 AuthRouter.get("/users", getUsers);
 AuthRouter.post("/register", register);
 AuthRouter.post("/login", login);
+AuthRouter.post("/get/:id", AUTH_MW, getUserInfo);
 
 module.exports = AuthRouter;
