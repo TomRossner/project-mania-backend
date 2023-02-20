49e4d60eaa83b72f110f2db8e91612d4f92ca24a
Tom Rossner <tomrossner2@gmail.com>
2023-02-21 00:42:53 +0200

    Update

--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -3,7 +3,7 @@ const ProjectRouter = express.Router();
 const {getProjects, addProject, updateProject, getTask} = require("../controllers/project.controllers");
 const AUTH_MW = require('../middlewares/auth.middleware');
 
-ProjectRouter.get("/all", AUTH_MW, getProjects);
+ProjectRouter.get("/:id/all", AUTH_MW, getProjects);
 ProjectRouter.post("/add", AUTH_MW, addProject);
 ProjectRouter.put("/:id", AUTH_MW, updateProject);
 ProjectRouter.get("/:id/:task_id", AUTH_MW, getTask);
