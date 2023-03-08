dfa59a93da47f9f301bd1384b3e44d89fbd10b54
Tom Rossner <tomrossner2@gmail.com>
2023-03-08 18:59:31 +0200

    Add deleteProject function

--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ProjectRouter = express.Router();
-const {getProjects, addProject, updateProject, getTask, deleteTask} = require("../controllers/project.controllers");
+const {getProjects, addProject, updateProject, getTask, deleteTask, deleteProject} = require("../controllers/project.controllers");
 const AUTH_MW = require('../middlewares/auth.middleware');
 
 ProjectRouter.get("/:id/all", AUTH_MW, getProjects);
@@ -8,5 +8,6 @@ ProjectRouter.post("/add", AUTH_MW, addProject);
 ProjectRouter.put("/:id", AUTH_MW, updateProject);
 ProjectRouter.get("/:id/:task_id", AUTH_MW, getTask);
 ProjectRouter.delete("/:id/:stage_id/:task_id", AUTH_MW, deleteTask);
+ProjectRouter.delete("/delete/:id", AUTH_MW, deleteProject);
 
 module.exports = ProjectRouter;
